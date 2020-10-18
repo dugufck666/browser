3c140058f307dbeb45bbd8ccf0c7c42b3c260199
damo <235065@qq.com>
2020-10-18 16:40:33 +0800

    开发版本v0.9

--- app.js
+++ app.js
@@ -4,22 +4,32 @@
  * @Author: Damo
  * @Date: 2020-10-14 10:42:39
  * @LastEditors: Damo
- * @LastEditTime: 2020-10-14 11:36:39
+ * @LastEditTime: 2020-10-18 16:36:37
  */
 const puppeteer = require('puppeteer');
 (async () => {
   //打开浏览器,安装模块中的chrome浏览器
   const browser = await puppeteer.launch({
     //false为有界面，true为无界面
-    headless: false
+    headless: false,
+    defaultViewport: {
+      width: 1920, //设置视窗的宽高
+      height: 800
+    },
   });
+
   //打开新的标签页
   const page = await browser.newPage();
+  //设置可视区域大小
+  await page.setViewport({
+    width: 1920,
+    height: 800
+  });
   //将打开的标签页跳转到百度首页。
   await page.goto('https://baidu.com');
-  //在百度搜索输入框中输入 "Node.js" 关键字
+  //在百度搜索输入框中输入 "前端神器" 关键字
   //page.type在dom元素中输入字符串，delay: 100,输入每个字符间隔100毫秒
-  await page.type('#kw', 'Node.js', {
+  await page.type('#kw', '前端神器', {
     delay: 100
   });
   //执行点击搜索按钮
@@ -28,32 +38,33 @@ const puppeteer = require('puppeteer');
   //waitForSelector()等待dom元素加载完成
   //waitForTimeout()等待多少毫秒
   await page.waitForSelector('.result');
-  //在搜索结果中遍历标题包含“菜鸟教程”关键字的链接
+  //在搜索结果中遍历标题包含“博客园”关键字的链接
   //page.evaluate注入函数到页面中，并返回函数处理的结果对象
   const targetLink = await page.evaluate(() => {
-    return {
-      link: function () {
-        var hrefs = [...document.querySelectorAll('.result a')];
-        var href = hrefs.filter(item => {
-          //取搜索结果中 标题包含“菜鸟教程”关键字的链接
-          return item.innerText && item.innerText.includes('菜鸟教程')
-        })[0];//如果结果有多条，只取第1条，并转为string返回
-        //console.log会输出结果到浏览器调试
-        //console.log(href);
-        //a标签dom对象toString(),输出的是href的值
-        return href.toString();
-      }(),
-      hrefs: function () {
-        var hrefs = [...document.querySelectorAll('.result a')];
-        return hrefs;
-      }()
-    }
-
+    return [...document.querySelectorAll('.result a')].filter(item => {
+      //取搜索结果中 标题包含“博客园”关键字的链接
+      return item.innerText && item.innerText.includes('博客园')
+    })[0].toString();
   });
   //console.log(targetLink);
   //当前页面跳转到搜索结果返回的链接
-  await page.goto(targetLink.link);
-  await page.waitForTimeout(1000);
+  await page.goto(targetLink);
+  //等待mainContent元素载入
+  await page.waitForSelector('#mainContent');
+  //网页截图
+  await page.screenshot({
+    path: './screen/capture.png', //图片保存路径
+    type: 'png',
+    fullPage: true //边滚动边截图
+    // clip: {x: 0, y: 0, width: 1920, height: 800}
+  });
+  //对网页元素截图
+  let [element] = await page.$x('//*[@id="sidebar_news"]/h3');
+  await element.screenshot({
+    path: './screen/element.png'
+  });
+  //关闭标签页
+  await page.close();
   //关键浏览器
-  browser.close();
+  await browser.close();
 })()
